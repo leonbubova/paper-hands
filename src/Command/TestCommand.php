ed0e777a541bcef3ed0a86e2e72f0d1805894eeb
bubovaleon <bubova.leon@gmx.de>
2021-03-14 14:33:46 +0100

    add new test:command to quickly test implemmentationsW

---
+++ src/Command/TestCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class TestCommand extends Command
+{
+    protected static $defaultName = 'test:command';
+    protected static $defaultDescription = 'Add a short description for your command';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription(self::$defaultDescription)
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->success('You have a new command!');
+
+        return Command::SUCCESS;
+    }
+}
