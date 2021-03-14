591cb0c645985f1f82068d7dde5b43bf2d09fe25
bubovaleon <bubova.leon@gmx.de>
2021-03-14 14:33:17 +0100

    remove old migrations and create one anew

--- migrations/Version20210313231157.php
+++ migrations/Version20210313231157.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210312223819 extends AbstractMigration
+final class Version20210313231157 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20210312223819 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE portfolio (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE portfolio (id INT AUTO_INCREMENT NOT NULL, balance INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE position (id INT AUTO_INCREMENT NOT NULL, portfolio_id INT NOT NULL, ticker VARCHAR(255) NOT NULL, amount INT NOT NULL, average_price INT NOT NULL, INDEX IDX_462CE4F5B96B5643 (portfolio_id), UNIQUE INDEX UNIQ_462CE4F5B96B56437EC30896 (portfolio_id, ticker), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE position ADD CONSTRAINT FK_462CE4F5B96B5643 FOREIGN KEY (portfolio_id) REFERENCES portfolio (id)');
     }
